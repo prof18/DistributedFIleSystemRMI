5bd066d2d25bdd0080be95ab87c0e97fbed20cfd
zigio <aziggiotto@gmail.com>
2018-02-20 16:08:52 +0100

    Prova della connessione tra PC

--- src/HelloImpl.java
+++ src/HelloImpl.java
@@ -3,6 +3,7 @@ import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class HelloImpl extends UnicastRemoteObject implements  Hello{
@@ -10,10 +11,11 @@ public class HelloImpl extends UnicastRemoteObject implements  Hello{
         super();
     }
     public String sayHello() throws RemoteException{
+        System.out.println("hai salutato qualcuno");
         return "Ciao bello";
     }
     public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
         Hello hello=new HelloImpl();
-        Naming.bind("//localhost/hello",hello);
+        Naming.bind("/localhost/hello",hello);
     }
 }
--- src/Prova.java
+++ src/Prova.java
@@ -2,10 +2,12 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Prova {
     public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
-        Hello hello =(Hello)Naming.lookup("//localhost/hello");
+        Hello hello=(Hello)Naming.lookup("/localhost/hello");
         System.out.println(hello.sayHello());
     }
 }
