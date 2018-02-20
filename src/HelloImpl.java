c78676914d56eb9893dca344b65706aaff1cdb12
zigio <aziggiotto@gmail.com>
2018-02-20 17:32:04 +0100

    Connessione funzionante senza porta

--- src/HelloImpl.java
+++ src/HelloImpl.java
@@ -7,15 +7,16 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class HelloImpl extends UnicastRemoteObject implements Hello {
+
     public HelloImpl() throws RemoteException {
         super();
     }
 
     public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
+        System.setProperty("java.rmi.server.hostname", "10.8.0.4");
         String ip="10.8.0.4";
         int port=1099;
         Hello hello = new HelloImpl();
-        System.setProperty("java.rmi.server.hostname", ip);
         Registry registry = LocateRegistry.createRegistry(port);
         String connectPath = "rmi://" + ip + "/" + "hello";
         System.out.println(connectPath);
