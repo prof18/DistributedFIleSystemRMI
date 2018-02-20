1db40bd0f0544f210f5f01a4621f2f5fb6e9abf7
ilDavide <thechryst92@gmail.com>
2018-02-20 15:46:51 +0100

    Interfaccia flat service e classe file attribute

--- src/FlatService.java
+++ src/FlatService.java
@@ -2,6 +2,16 @@ import java.io.Serializable;
 import java.rmi.Remote;
 
 public interface FlatService extends Remote, Serializable {
+
+    Object read(String fileID, int i, int n);
+
+    void write(String fileID, int i, Object data);
+
     void Create();
-    void Delete(int fileID);
+
+    void Delete(String fileID);
+
+    FileAttribute getAttributes(String fileID);
+
+    void setAttributes(String fileID, FileAttribute attr);
 }
