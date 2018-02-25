41ba14e5a1886f51d39045ef5d18a3860997d8ad
Marco Gomiero <marco.gomiero.93@gmail.com>
2018-02-25 17:08:05 +0100

    tree view and info view are ready

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
@@ -38,87 +39,53 @@ public class MainUI extends JFrame {
         filesUI.setBackground(Color.BLUE);
         //File Details
         JPanel filesDetail = createDetailsUI();
-        filesDetail.setBackground(Color.YELLOW);
+        //filesDetail.setBackground(Color.YELLOW);
         setLayout(new GridBagLayout());
-
-        DefaultMutableTreeNode top =
-                new DefaultMutableTreeNode("The Java Series");
+        //Tree View
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("The Java Series");
         createNodes(top);
-
-        //Create a tree that allows one selection at a time.
         JTree tree = new JTree(top);
-        tree.getSelectionModel().setSelectionMode
-                (TreeSelectionModel.SINGLE_TREE_SELECTION);
+        //Only One Selection
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 
-        //Listen for when the selection changes.
-        //tree.addTreeSelectionListener(this);
+        tree.addTreeSelectionListener((TreeSelectionListener) -> {
+            System.out.println("Item Selected");
+        });
+
+        tree.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         JScrollPane treeScroll = new JScrollPane(tree);
 
+        GridBagConstraints globalCS = new GridBagConstraints();
+        GridBagConstraints rwCS = new GridBagConstraints();
 
-        // as per trashgod tip
-    //    tree.setVisibleRowCount(15);
+        rwCS.weightx = 1;
+        rwCS.weighty = 0.95;
+        rwCS.gridx = 0;
+        rwCS.gridy = 0;
+        rwCS.fill = GridBagConstraints.BOTH;
+        rightWrapper.add(filesUI, rwCS);
 
-        /*Dimension preferredSize = treeScroll.getPreferredSize();
-        Dimension widePreferred = new Dimension(
-                200,
-                (int)preferredSize.getHeight());
-        treeScroll.setPreferredSize( widePreferred );
-*/
+        rwCS.weightx = 1;
+        rwCS.weighty = 0.05;
+        rwCS.gridx = 0;
+        rwCS.gridy = 1;
+        rwCS.fill = GridBagConstraints.BOTH;
+        rightWrapper.add(filesDetail, rwCS);
 
+        globalCS.weighty = 1;
+        globalCS.weightx = 0.05;
+        globalCS.gridx = 0;
+        globalCS.gridy = 0;
+        globalCS.fill = GridBagConstraints.BOTH;
+        add(treeScroll, globalCS);
 
-
-        GridBagConstraints c = new GridBagConstraints();
-        GridBagConstraints c1 = new GridBagConstraints();
-
-        c1.gridx = 0;
-        c1.gridy = 0;
-        c1.fill = GridBagConstraints.BOTH;
-        rightWrapper.add(filesUI, c1);
-
-        c1.gridx = 0;
-        c1.gridy = 1;
-        c1.fill = GridBagConstraints.BOTH;
-        rightWrapper.add(filesDetail, c1);
-
-        c.gridx = 0;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
-        add(treeScroll, c);
-
-        c.gridx = 1;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
-        add(rightWrapper, c);
-
-      /*  c.gridx = 0;
-       // c.weighty = 1;
-
-        c.weightx = 0.2;
-    //    c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
-        add(treeScroll, c);*/
-
-/*      c1.fill = GridBagConstraints.BOTH;
-      c1.weighty = 1;
-      c1.gridheight = 1;
-      add(treeScroll, c1);
-
-        c.weightx = 0.8;
-        // Files UI
-        c.gridx = 1;
-        c.weighty = 0.9;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
-        add(filesUI, c);
-
-        //Details UI
-        c.gridx = 1;
-        c.weighty = 0.1;
-        c.gridy = 1;
-        c.fill = GridBagConstraints.BOTH;
-        c.anchor = GridBagConstraints.WEST;
-        add(filesDetail, c);*/
+        globalCS.weighty = 1;
+        globalCS.weightx = 0.95;
+        globalCS.gridx = 1;
+        globalCS.gridy = 0;
+        globalCS.fill = GridBagConstraints.BOTH;
+        add(rightWrapper, globalCS);
 
         //TODO: Enable the settings pop-up
 /*
@@ -131,17 +98,16 @@ public class MainUI extends JFrame {
 
         JPanel filesDetail = new JPanel(new GridBagLayout());
         GridBagConstraints cs = new GridBagConstraints();
-        //cs.fill = GridBagConstraints.VERTICAL;
-        cs.insets = new Insets(5, 10, 5, 10);
-
-        //cs.anchor = GridBagConstraints.EAST;
+        cs.insets = new Insets(5, 10, 5, 70);
 
         //File Name
         JLabel fileNameLabel = new JLabel("File Name: ");
+        fileNameLabel.setOpaque(true);
         cs.gridx = 0;
         cs.gridy = 0;
         filesDetail.add(fileNameLabel, cs);
         fileNameVLabel = new JLabel("LR Exam Schedule");
+        //cs.fill = GridBagConstraints.BOTH;
         cs.gridx = 1;
         cs.gridy = 0;
         filesDetail.add(fileNameVLabel, cs);
@@ -196,67 +162,73 @@ public class MainUI extends JFrame {
         cs.gridy = 5;
         filesDetail.add(lastEditVLabel, cs);
 
+        //Second Column
+
+        //File Name
+        JLabel fileNameLabel1 = new JLabel("File Name: ");
+        cs.gridx = 2;
+        cs.gridy = 0;
+        filesDetail.add(fileNameLabel1, cs);
+        fileNameVLabel = new JLabel("LR Exam Schedule");
+        cs.gridx = 3;
+        cs.gridy = 0;
+        filesDetail.add(fileNameVLabel, cs);
+
+        //Type
+        JLabel typeLabel1 = new JLabel("Type: ");
+        cs.gridx = 2;
+        cs.gridy = 1;
+        filesDetail.add(typeLabel1, cs);
+        typeVLabel = new JLabel("file");
+        cs.gridx = 3;
+        cs.gridy = 1;
+        filesDetail.add(typeVLabel, cs);
+
+        //Path
+        JLabel pathLabel1 = new JLabel("Path: ");
+        cs.gridx = 2;
+        cs.gridy = 2;
+        filesDetail.add(pathLabel1, cs);
+        pathVLabel = new JLabel("/home/luca/ansia/");
+        cs.gridx = 3;
+        cs.gridy = 2;
+        filesDetail.add(pathVLabel, cs);
+
+        //File Size
+        JLabel fileSizeLabel1 = new JLabel("File Size: ");
+        cs.gridx = 2;
+        cs.gridy = 3;
+        filesDetail.add(fileSizeLabel1, cs);
+        fileSizeVLabel = new JLabel("35kb");
+        cs.gridx = 3;
+        cs.gridy = 3;
+        filesDetail.add(fileSizeVLabel, cs);
+
+        //Owner
+        JLabel ownerLabel1 = new JLabel("Owner: ");
+        cs.gridx = 2;
+        cs.gridy = 4;
+        filesDetail.add(ownerLabel1, cs);
+        ownerVLabel = new JLabel("LR18");
+        cs.gridx = 3;
+        cs.gridy = 4;
+        filesDetail.add(ownerVLabel, cs);
+
+        //Last Edit
+        JLabel lastEditLabel1 = new JLabel("Last Edit: ");
+        cs.gridx = 2;
+        cs.gridy = 5;
+        filesDetail.add(lastEditLabel1, cs);
+        lastEditVLabel = new JLabel("Thu Feb 24 17:34:55");
+        cs.gridx = 3;
+        cs.gridy = 5;
+        filesDetail.add(lastEditVLabel, cs);
+
         return filesDetail;
 
     }
 
-
-
-
-
-        public JTree TreeDemo() {
-
-
-            DefaultMutableTreeNode top =
-                    new DefaultMutableTreeNode("The Java Series");
-            createNodes(top);
-
-            //Create a tree that allows one selection at a time.
-            JTree tree = new JTree(top);
-            tree.getSelectionModel().setSelectionMode
-                    (TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-            //Listen for when the selection changes.
-            //tree.addTreeSelectionListener(this);
-
-
-            JScrollPane treeScroll = new JScrollPane(tree);
-
-            // as per trashgod tip
-            tree.setVisibleRowCount(15);
-
-            Dimension preferredSize = treeScroll.getPreferredSize();
-            Dimension widePreferred = new Dimension(
-                    200,
-                    (int)preferredSize.getHeight());
-            treeScroll.setPreferredSize( widePreferred );
-
-            //Create the scroll pane and add the tree to it.
-            //JScrollPane treeView = new JScrollPane(tree);
-
- /*           //Create the HTML viewing pane.
-            JEditorPane htmlPane = new JEditorPane();
-            htmlPane.setEditable(false);
-            //initHelp();
-            JScrollPane htmlView = new JScrollPane(htmlPane);
-
-            //Add the scroll panes to a split pane.
-            JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-            splitPane.setTopComponent(treeView);
-            splitPane.setBottomComponent(htmlView);
-
-            Dimension minimumSize = new Dimension(100, 50);
-            htmlView.setMinimumSize(minimumSize);
-            treeView.setMinimumSize(minimumSize);
-            splitPane.setDividerLocation(100);
-            splitPane.setPreferredSize(new Dimension(500, 300));
-*/
-            //Add the split
-            return tree;
-        }
-
-
-        private void createNodes(DefaultMutableTreeNode top) {
+    private void createNodes(DefaultMutableTreeNode top) {
         DefaultMutableTreeNode category = null;
         DefaultMutableTreeNode book = null;
 
@@ -328,7 +300,9 @@ public class MainUI extends JFrame {
         //Settings
         menuItem = new JMenuItem("Settings");
         menuItem.addActionListener((ActionListener) -> {
-            System.out.println("CLicked Settings");
+            SettingsDialog settingsDialog = new SettingsDialog(this);
+            settingsDialog.setVisible(true);
+
         });
         menu.add(menuItem);
         //About
