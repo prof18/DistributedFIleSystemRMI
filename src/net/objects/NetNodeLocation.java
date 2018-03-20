b669dab22f8248c9155678572c272a0510368c5c
luca <rossi.luca00@gmail.com>
2018-03-20 22:38:58 +0100

    hostName random

--- src/net/objects/NetNodeLocation.java
+++ src/net/objects/NetNodeLocation.java
@@ -1,12 +1,15 @@
 package net.objects;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
 public class NetNodeLocation implements Serializable {
     private String ip;
     private int port;
     private String name;
+
+    private long timeStamp = new Date().getTime();
 
     public NetNodeLocation(String ip, int port, String name) {
         this.ip = ip;
@@ -18,6 +21,8 @@ public class NetNodeLocation implements Serializable {
     public String getName() {
         return name;
     }
+
+    public long getTimeStamp(){return timeStamp;}
 
     public void setName(String name){this.name = name;}
 
