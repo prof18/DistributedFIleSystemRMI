74c69484554aa4c9f685056869cc60d381379278
Marco Gomiero <marco.gomiero.93@gmail.com>
2018-03-26 11:13:49 +0200

    added timestamp on JsonFolder

--- src/fs/objects/json/JsonFolder.java
+++ src/fs/objects/json/JsonFolder.java
@@ -4,82 +4,91 @@ import java.util.ArrayList;
 
 public class JsonFolder {
 
-   private String UFID;
-   private boolean isRoot;
-   private String folderName;
-   private String parentUFID;
-   private ArrayList<String> children;
-   private ArrayList<JsonFile> files;
+    private String UFID;
+    private long lastEditTime;
+    private boolean isRoot;
+    private String folderName;
+    private String parentUFID;
+    private ArrayList<String> children;
+    private ArrayList<JsonFile> files;
 
-   public JsonFolder() {
-   }
+    public JsonFolder() {
+    }
 
-   public JsonFolder(String UFID, boolean isRoot, String folderName, String parentUFID, ArrayList<String> children, ArrayList<JsonFile> files) {
-      this.UFID = UFID;
-      this.isRoot = isRoot;
-      this.folderName = folderName;
-      this.parentUFID = parentUFID;
-      this.children = children;
-      this.files = files;
-   }
+    public JsonFolder(String UFID, boolean isRoot, String folderName, String parentUFID, ArrayList<String> children, ArrayList<JsonFile> files) {
+        this.UFID = UFID;
+        this.isRoot = isRoot;
+        this.folderName = folderName;
+        this.parentUFID = parentUFID;
+        this.children = children;
+        this.files = files;
+    }
 
-   public String getUFID() {
-      return UFID;
-   }
+    public String getUFID() {
+        return UFID;
+    }
 
-   public void setUFID(String UFID) {
-      this.UFID = UFID;
-   }
+    public void setUFID(String UFID) {
+        this.UFID = UFID;
+    }
 
-   public boolean isRoot() {
-      return isRoot;
-   }
+    public long getLastEditTime() {
+        return lastEditTime;
+    }
 
-   public void setRoot(boolean root) {
-      isRoot = root;
-   }
+    public void setLastEditTime(long lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
 
-   public String getFolderName() {
-      return folderName;
-   }
+    public boolean isRoot() {
+        return isRoot;
+    }
 
-   public void setFolderName(String folderName) {
-      this.folderName = folderName;
-   }
+    public void setRoot(boolean root) {
+        isRoot = root;
+    }
 
-   public String getParentUFID() {
-      return parentUFID;
-   }
+    public String getFolderName() {
+        return folderName;
+    }
 
-   public void setParentUFID(String parentUFID) {
-      this.parentUFID = parentUFID;
-   }
+    public void setFolderName(String folderName) {
+        this.folderName = folderName;
+    }
 
-   public ArrayList<String> getChildren() {
-      return children;
-   }
+    public String getParentUFID() {
+        return parentUFID;
+    }
 
-   public void setChildren(ArrayList<String> children) {
-      this.children = children;
-   }
+    public void setParentUFID(String parentUFID) {
+        this.parentUFID = parentUFID;
+    }
 
-   public ArrayList<JsonFile> getFiles() {
-      return files;
-   }
+    public ArrayList<String> getChildren() {
+        return children;
+    }
 
-   public void setFiles(ArrayList<JsonFile> files) {
-      this.files = files;
-   }
+    public void setChildren(ArrayList<String> children) {
+        this.children = children;
+    }
 
-   @Override
-   public String toString() {
-      return "JsonFolder{" +
-              "UFID='" + UFID + '\'' +
-              ", isRoot=" + isRoot +
-              ", folderName='" + folderName + '\'' +
-              ", parentUFID='" + parentUFID + '\'' +
-              ", children=" + children +
-              ", files=" + files +
-              '}';
-   }
+    public ArrayList<JsonFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(ArrayList<JsonFile> files) {
+        this.files = files;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonFolder{" +
+                "UFID='" + UFID + '\'' +
+                ", isRoot=" + isRoot +
+                ", folderName='" + folderName + '\'' +
+                ", parentUFID='" + parentUFID + '\'' +
+                ", children=" + children +
+                ", files=" + files +
+                '}';
+    }
 }
