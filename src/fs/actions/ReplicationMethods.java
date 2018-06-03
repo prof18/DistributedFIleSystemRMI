ebed9c75848ceb25581e901957290dd7886d6baa
zigio <aziggiotto@gmail.com>
2018-06-03 17:43:28 +0200

    last minuti

--- src/fs/actions/ReplicationMethods.java
+++ src/fs/actions/ReplicationMethods.java
@@ -64,12 +64,15 @@ public class ReplicationMethods {
         }
 
         try {
-            for(Map.Entry<Integer,NetNodeLocation> entry:nNode.getHashMap().entrySet()){
-                Registry registry1=LocateRegistry.getRegistry(entry.getValue().getIp(),entry.getValue().getPort());
-                NetNode node=(NetNode)registry1.lookup(entry.getValue().toUrl());
-                node.removeFromCache(repWr.getUFID());
+            for (Map.Entry<Integer, NetNodeLocation> entry : nNode.getHashMap().entrySet()) {
+                if (!nNode.getFileNodeList().get(repWr.getUFID()).getLocations().contains(entry.getValue())) {
+                    Registry registry1 = LocateRegistry.getRegistry(entry.getValue().getIp(), entry.getValue().getPort());
+                    NetNode node = (NetNode) registry1.lookup(entry.getValue().toUrl());
+                    node.removeFromCache(repWr.getUFID());
 
+                }
             }
+
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (NotBoundException e) {
@@ -78,67 +81,68 @@ public class ReplicationMethods {
     }
 
 
-    /**
-     * This method is used to replicate file system structure in all connected nodes
-     *
-     * @param netNode node where file system JSON is replicated
-     * @param root    Root of the file system
-     */
-    public void jsonReplication(NetNodeLocation netNode, FSTreeNode root) {
-        Registry registry;
-        try {
-            registry = LocateRegistry.getRegistry(netNode.getIp(), netNode.getPort());
-            NetNode node = (NetNode) registry.lookup(netNode.toUrl());
-            node.updateUI(root);
-
-        } catch (RemoteException | NotBoundException e) {
-            System.out.println("[FILE-REPLICATION] Communication problem");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * This method is used to delete local replicated files in the connected nodes
-     *
-     * @param fileID            file UFID
-     * @param netNode           node where file is local saved
-     * @param treeFileDirectory Directory to delete
-     */
-    public boolean deleteFile(String fileID, NetNodeLocation netNode, FSTreeNode treeFileDirectory, long fileSize) {
-        boolean deleteState = false;
-        Registry registry;
-        try {
-            registry = LocateRegistry.getRegistry(netNode.getIp(), netNode.getPort());
-            NetNode node = (NetNode) registry.lookup(netNode.toUrl());
-
-            String directoryUFID = null;
-            if (treeFileDirectory != null) {
-                directoryUFID = treeFileDirectory.getUFID();
-            }
-
-            if (node.deleteFile(fileID, directoryUFID, fileSize)) {
-                deleteState = true;
-            }
-
-        } catch (RemoteException | NotBoundException e) {
-            System.out.println("[FILE-REPLICATION] Communication problem");
-            e.printStackTrace();
-        }
-
-        return deleteState;
-    }
-
-    public void updateWritePermissionMap(String fileID, Collection<NetNodeLocation> nodeSet, ListFileWrapper listFileWrapper) {
-        Registry registry;
-        try {
-            for (NetNodeLocation netNode : nodeSet) {
+        /**
+         * This method is used to replicate file system structure in all connected nodes
+         *
+         * @param netNode node where file system JSON is replicated
+         * @param root    Root of the file system
+         */
+        public void jsonReplication (NetNodeLocation netNode, FSTreeNode root){
+            Registry registry;
+            try {
                 registry = LocateRegistry.getRegistry(netNode.getIp(), netNode.getPort());
                 NetNode node = (NetNode) registry.lookup(netNode.toUrl());
-                node.updateWritePermissionMap(fileID, listFileWrapper);
+                node.updateUI(root);
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("[FILE-REPLICATION] Communication problem");
+                e.printStackTrace();
             }
-        } catch (RemoteException | NotBoundException e) {
-            System.out.println("[FILE-REPLICATION] Communication problem");
-            e.printStackTrace();
+        }
+
+        /**
+         * This method is used to delete local replicated files in the connected nodes
+         *
+         * @param fileID            file UFID
+         * @param netNode           node where file is local saved
+         * @param treeFileDirectory Directory to delete
+         */
+        public boolean deleteFile (String fileID, NetNodeLocation netNode, FSTreeNode treeFileDirectory,long fileSize){
+            boolean deleteState = false;
+            Registry registry;
+            try {
+                registry = LocateRegistry.getRegistry(netNode.getIp(), netNode.getPort());
+                NetNode node = (NetNode) registry.lookup(netNode.toUrl());
+
+                String directoryUFID = null;
+                if (treeFileDirectory != null) {
+                    directoryUFID = treeFileDirectory.getUFID();
+                }
+
+                if (node.deleteFile(fileID, directoryUFID, fileSize)) {
+                    deleteState = true;
+                }
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("[FILE-REPLICATION] Communication problem");
+                e.printStackTrace();
+            }
+
+            return deleteState;
+        }
+
+        public void updateWritePermissionMap (String fileID, Collection < NetNodeLocation > nodeSet, ListFileWrapper
+        listFileWrapper){
+            Registry registry;
+            try {
+                for (NetNodeLocation netNode : nodeSet) {
+                    registry = LocateRegistry.getRegistry(netNode.getIp(), netNode.getPort());
+                    NetNode node = (NetNode) registry.lookup(netNode.toUrl());
+                    node.updateWritePermissionMap(fileID, listFileWrapper);
+                }
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("[FILE-REPLICATION] Communication problem");
+                e.printStackTrace();
+            }
         }
     }
-}
